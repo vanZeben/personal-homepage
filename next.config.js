f8a8204b29604e19bdecb8b02c9dac911d057e18
Ryan van Zeben <ryan.vanzeben@prodigygame.com>
2021-02-04 13:40:13 -0500

    Redirect to profile

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const debug = process.env.NODE_ENV !== "production";
-
-module.exports = {
-  distDir: 'build',
-  assetPrefix: !debug ? '/personal-homepage' : '',
-  basePath: !debug ? '/personal-homepage' : '',
-}
